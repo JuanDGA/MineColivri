65744c9d82038ea8a737ef90f394b2b8018aa847
Juan Guevara <soyjuandavidguevara@gmail.com>
2025-05-10 12:56:32 -0500

    Plugin initialization

---
+++ src/main/kotlin/dev/jguevara/minecolivri/Minecolivri.kt
@@ -0,0 +1,14 @@
+package dev.jguevara.minecolivri
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class Minecolivri : JavaPlugin() {
+
+  override fun onEnable() {
+    // Plugin startup logic
+  }
+
+  override fun onDisable() {
+    // Plugin shutdown logic
+  }
+}
